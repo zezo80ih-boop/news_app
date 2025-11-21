5e269a0a99f29883dd1ee0de67a899da6efc3757
Ahmed <zezo80ih@gmail.com>
2025-11-21 22:26:12 +0200

    start

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
